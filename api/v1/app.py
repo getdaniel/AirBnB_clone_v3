aa2b78604c41789fdb99012b46c45056bac19eba
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-21 14:11:31 +0300

    status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 '''Flask App Blueprint Setup'''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from models import storage
 from api.v1.views import app_views
-import os
+from flask import Flask
+from os import getenv
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardowni_db(exception):
     '''Closes connection to storage'''
     if storage is not None:
         storage.close()
 
 
-@app.errorhandler(404)
-def not_found(error):
-    """Error handler for the 404"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
