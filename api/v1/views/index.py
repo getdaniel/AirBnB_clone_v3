b6357cf4f3ceb87aac80a87e9c20da6d7412361c
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-21 16:35:11 +0300

    update to get correct result

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-classes = {"amenities": Amenity, "cities": City, "places": Place,
-           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', methods=['GET'])
@@ -22,10 +13,18 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'])
-def return_stats():
+def stats():
     '''returns a count of the instances of a class'''
-    new_dict = {}
-    for c in classes:
-        count = storage.count(classes[c])
-        new_dict[c] = count
-        return jsonify(new_dict)
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
