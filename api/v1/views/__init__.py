e8872ba6a064a7ec8d6fb2cedd2336c3ec6a0ac9
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-21 15:39:32 +0300

    fix the conflic

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,3 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-<<<<<<< HEAD
 from api.v1.views.index import * # wildcard import
-=======
-from api.v1.views.index import *
->>>>>>> storage_get_count
