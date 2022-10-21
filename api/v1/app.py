d011b2e581da135b60131573a0fd9c8c828e2182
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-21 15:55:52 +0300

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Flask App Blueprint Setup'''
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import getenv
 from models import storage
 
@@ -14,6 +14,13 @@ def teardown_db(exception):
     '''Closes connection to storage'''
     if storage is not None:
         storage.close()
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
