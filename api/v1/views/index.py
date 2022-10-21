c88c1210c12a2921c5af1131c5d84e283acea11f
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-21 16:05:49 +0300

    fix the betty code style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,16 @@
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
-from models.amenity import Amenity		
-from models.city import City		
-from models.place import Place		
-from models.review import Review		
-from models.state import State		
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 classes = {"amenities": Amenity, "cities": City, "places": Place,
            "reviews": Review, "states": State, "users": User}
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
@@ -19,7 +20,8 @@ def status():
     if request.method == 'GET':
         return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])		
+
+@app_views.route('/stats', methods=['GET'])
 def return_stats():
     '''returns a count of the instances of a class'''
     new_dict = {}
